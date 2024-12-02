3f76b478a20ab619a46080b3f588344f1236c9c3
kimenyu <njorogekimenyu@gmail.com>
2024-12-02 17:06:37 +0300

    feat: Add SQL queries for aggregations and window functions
    - Created `aggregations_and_window_functions.sql` to demonstrate SQL aggregations and window functions.
    - Aggregation query calculates total bookings by each user.
    - Window function query ranks properties based on total bookings.

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,36 +1,36 @@
 -- 1. Aggregation: Find the total number of bookings made by each user.
-SELECT 
+SELECT
     User.user_id,
     User.first_name,
     User.last_name,
     COUNT(Booking.booking_id) AS total_bookings
-FROM 
+FROM
     User
-LEFT JOIN 
+LEFT JOIN
     Booking ON User.user_id = Booking.user_id
-GROUP BY 
+GROUP BY
     User.user_id, User.first_name, User.last_name
-ORDER BY 
+ORDER BY
     total_bookings DESC;
 
--- 2. Window function: Rank properties based on the total number of bookings.
+-- 2. Window function: Rank properties based on the total number of bookings using ROW_NUMBER.
 WITH PropertyBookingCount AS (
-    SELECT 
+    SELECT
         Property.property_id,
         Property.name,
         COUNT(Booking.booking_id) AS booking_count
-    FROM 
+    FROM
         Property
-    LEFT JOIN 
+    LEFT JOIN
         Booking ON Property.property_id = Booking.property_id
-    GROUP BY 
+    GROUP BY
         Property.property_id, Property.name
 )
-SELECT 
+SELECT
     property_id,
     name,
     booking_count,
-    RANK() OVER (ORDER BY booking_count DESC) AS rank
-FROM 
+    ROW_NUMBER() OVER (ORDER BY booking_count DESC) AS rank
+FROM
     PropertyBookingCount;
 
